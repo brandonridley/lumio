85ee2fbf805f126f48ea4f7991a9077bc972bc92
brandonridley <me@brandonridley.com>
2025-08-30 18:44:44 -0700

    Create page.tsx

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: 24}}>
+      <h1>Lumio</h1>
+      <p>Frontend is up. API should be at <code>/hello</code> on api subdomain.</p>
+    </main>
+  );
+}
